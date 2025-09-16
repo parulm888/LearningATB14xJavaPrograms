00d62fed9b3a55e2bd0eb22d6c589a039ad4e4fa
parul meena <Parul.m888@gmail.com>
2025-09-17 01:47:48 +0530

    Boolean data type practice

---
+++ src/ex_01_Java_Basics/Lab005_Boolean_type_java.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Lab005_Boolean_type_java {
+    public static void main(String[] args) {
+        boolean is_Delhi$in$india = true;
+        System.out.println(is_Delhi$in$india);
+
+        boolean America_is$Selfish = true;
+        System.out.println(America_is$Selfish);
+
+    }
+}
