1f38d9434b283c2c8837bcadae22fb4631910992
parul meena <Parul.m888@gmail.com>
2025-09-17 00:56:35 +0530

    Data Types Practice

---
+++ src/ex_01_Java_Basics/Lab004_Byte_integral_java.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basics;
+
+public class Lab004_Byte_integral_java {
+    public static void main(String[] args) {
+        byte b =1;
+        Short y = 6;
+        float f1 = 45.4f;
+        int $3 = 78000;
+        char $4 = 'p';
+
+        System.out.println(b);
+        System.out.println(y);
+        System.out.println(f1);
+        System.out.println($3);
+        System.out.println($4);
+    }
+}
