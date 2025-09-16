2ba4193e9d7e65adad2bb628c774b5493f453e1c
parul meena <Parul.m888@gmail.com>
2025-09-17 03:58:34 +0530

    Operator tyoe practices

---
+++ src/ex_01_Java_Basics/Lab007_Arithmetic_Operators_Java.java
@@ -0,0 +1,24 @@
+package ex_01_Java_Basics;
+
+public class Lab007_Arithmetic_Operators_Java {
+    public static void main(String[] args) {
+        int a = 10;
+        int b =5;
+        int c = -8;
+        long l = 1450000;
+        int difference = a - b;
+        int plus = a + c;
+        long salary = l + a + b;
+        long multiply = a * l;
+        long divide = l / a;
+        int modulus = a % b;
+
+        System.out.println(difference);
+        System.out.println(plus);
+        System.out.println(salary);
+        System.out.println(multiply);
+        System.out.println(divide);
+        System.out.println(modulus);
+
+    }
+}
