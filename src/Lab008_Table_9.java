42c02f30bf72b3426fcb2778d8231f5b6d800e2e
parul meena <Parul.m888@gmail.com>
2025-09-18 03:17:48 +0530

    Assignment_Table of 9

---
+++ src/Lab008_Table_9.java
@@ -0,0 +1,24 @@
+public class Lab008_Table_9 {
+    public static void main(String[] args) {
+        int a = 9;
+        System.out.printf("%d*1=%d", a, a*1);
+        System.out.println();
+        System.out.printf("%d*1=%d", a, a*2);
+        System.out.println();
+        System.out.printf("%d*1=%d", a, a*3);
+        System.out.println();
+        System.out.printf("%d*1=%d", a, a*4);
+        System.out.println();
+        System.out.printf("%d*1=%d", a, a*5);
+        System.out.println();
+        System.out.printf("%d*1=%d", a, a*6);
+        System.out.println();
+        System.out.printf("%d*1=%d", a, a*7);
+        System.out.println();
+        System.out.printf("%d*1=%d", a, a*8);
+        System.out.println();
+        System.out.printf("%d*1=%d", a, a*9);
+        System.out.println();
+        System.out.printf("%d*1=%d", a, a*10);
+    }
+}
